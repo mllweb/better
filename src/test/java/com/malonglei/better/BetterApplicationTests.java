64fffca73182d5445fd4d0806933621c9727812b
malonglei <mllweb@163.com>
2019-01-25 09:50:43 +0800

    第一次提交

---
+++ src/test/java/com/malonglei/better/BetterApplicationTests.java
@@ -0,0 +1,17 @@
+package com.malonglei.better;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BetterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
